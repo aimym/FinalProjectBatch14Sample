6c67ba675dce53801d7bbf6009090e995abbd153
aimym <aidananuri25@gmail.com>
2024-02-13 12:01:13 -0500

    the basic solar system with the feature sun added is ready to be deployed

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,18 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SolarSystem Xron=new SolarSystem();
+        Xron.sun=1;
+        Xron.moon=2;
+        Xron.Planets=2;
+
+        FeatureSun Fxron=new FeatureSun();
+        Fxron.color="green";
+        Fxron.size="1234567km";
+        Fxron.temp="-4563729205";
+        
+    }
+}
